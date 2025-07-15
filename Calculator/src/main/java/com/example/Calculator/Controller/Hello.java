8f4cc30c912354d2e7ae6f3d72a6053c53d5b471
sowmithaakrishnan <sowmithaakrishnan@gmail.com>
2025-07-15 09:37:35 +0530

    Commit

---
+++ Calculator/src/main/java/com/example/Calculator/Controller/Hello.java
@@ -0,0 +1,9 @@
+package com.example.Calculator.Controller;
+
+
+
+public class Hello {
+    public String helloTest(){
+        return "This is test";
+    }
+}
---
+++ Calculator/src/test/java/com/example/Calculator/controller/HelloTest.java
@@ -0,0 +1,18 @@
+package com.example.Calculator.controller;
+
+
+
+import com.example.Calculator.Controller.Hello;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HelloTest {
+    @Test
+    public void testHelloTest(){
+        Hello hello = new Hello();
+        String s = hello.helloTest();
+        System.out.println(s);
+        assertEquals("This is test" , s);
+    }
+}
