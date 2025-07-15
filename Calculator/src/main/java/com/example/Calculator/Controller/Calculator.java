7df0d725e00fa3e47d75115d513f0537ca40af82
sowmithaakrishnan <sowmithaakrishnan@gmail.com>
2025-07-15 09:28:58 +0530

    Commit

---
+++ Calculator/src/main/java/com/example/Calculator/Controller/Calculator.java
@@ -0,0 +1,21 @@
+package com.example.Calculator.Controller;
+
+
+public class Calculator {
+
+    public int add(int a,int b){
+        return a+b;
+    }
+    public int subtract(int a,int b){
+        return a-b;
+    }
+    public int multiplication(int a,int b){
+        return a*b;
+    }
+    public int division(int a,int b){
+        return a/b;
+    }
+    public int modulo(int a,int b){
+        return a%b;
+    }
+}
